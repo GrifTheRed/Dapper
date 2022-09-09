1d24b344ff3b75cb64c3eb644a55a7a5f646b58c
Derek Griffin <griffinderek6@gmail.com>
2022-09-08 19:42:36 -0600

    setup dapper functionality

---
+++ DapperClass/Department.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace DapperClass
+{
+    public class Department
+    {
+        public Department()
+        {
+        }
+    }
+}
+
---
+++ DapperClass/IDepartmentRepository.cs
@@ -0,0 +1,8 @@
+﻿using System;
+namespace DapperClass
+{
+    public interface IDepartmentRepository
+    {
+    }
+}
+
